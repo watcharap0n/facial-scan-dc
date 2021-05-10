547e2f1f43a094751819e4bdc03469f9ffd7673d
watcharap0n <wera.watcharapon@gmail.com>
2021-05-10 15:36:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,18 +4,32 @@ import os
 
 
 app = Flask(__name__)
-
+app.secret_key = 'watcharaponweeraborirak'
 UPLOAD_FOLDER = 'static/uploads'
+
+
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
 
 @app.route('/face_rec', methods=['POST'])
 def face_rec():
     if 'file' not in request.files:
         flash('No file part')
-        return redirect(request.url)
+        return jsonify({'info': 'key form-data Invalid'})
     file = request.files['file']
-    if file.filename == '':
-        flash('No selected file')
-        return redirect(request.url)
     if file:
         image = os.path.join(UPLOAD_FOLDER, file.filename)
         file.save(image)
@@ -34,7 +48,6 @@ def face_rec():
             'url': url, 'status': 'success',
             'process_time': round(process_time, 2)
         })
-
 
 
 if __name__ == '__main__':
